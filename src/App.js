12e06f5438aebba5b83daa1ce9b7b74dd0ab2b32
nandumadhu <nandini06-tech@gmail.com>
2024-11-13 08:48:50 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
 import SimpleTodos from './components/SimpleTodos'
+import LoginForm from './components/LoginForm'
 
 import './App.css'
 
-const App = () => <SimpleTodos />
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/login" component={LoginForm} />
+
+      <Route path="/" component={SimpleTodos} />
+    </Routes>
+  </BrowserRouter>
+)
 
 export default App
